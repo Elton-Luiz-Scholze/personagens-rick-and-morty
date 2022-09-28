feadd5bd8df05f14b9207ac2c33a1672c3f8008f
Elton Luiz Scholze <elscholze@gmail.com>
2022-09-28 09:32:48 -0300

    fix: fix App.jsx

--- react-buscando-mais-personagens-elton-luiz-scholze/src/App.jsx
+++ react-buscando-mais-personagens-elton-luiz-scholze/src/App.jsx
@@ -4,17 +4,27 @@ import "./App.css";
 
 function App() {
   const [characterList, setCharacterList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
+    fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
       .then((res) => res.json())
       .then((res) => setCharacterList(res.results))
       .catch((err) => console.log(err));
-  }, []);
+  }, [currentPage]);
+
+  function previousPage() {
+    setCurrentPage(currentPage - 1);
+  }
+  function nextPage() {
+    setCurrentPage(currentPage + 1);
+  }
 
   return (
     <div className="App">
       <Character characterList={characterList} />
+      <button onClick={previousPage}>Previous</button>
+      <button onClick={nextPage}>Next</button>
     </div>
   );
 }
